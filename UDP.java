d63ccabe20fa862dc445d5a5de935e2d8bec764a
Dinesha G <120408570+Dine24@users.noreply.github.com>
2023-09-05 13:34:17 +0530

    Create UDP.java

---
+++ UDP.java
@@ -0,0 +1,38 @@
+Client Side:
+------------------------------------------------------------------------------------------------------
+ 
+import java.io.*;
+import java.net.*;
+ 
+class UDPClient
+{
+   public static void main(String args[]) throws Exception
+   {
+      BufferedReader inFromUser =
+         new BufferedReader(new InputStreamReader(System.in));
+ 
+      DatagramSocket clientSocket = new DatagramSocket();
+ //Client Socket is created
+ 
+      InetAddress IPAddress = InetAddress.getByName("localhost");
+ //Gets the IP Address 
+ 
+      byte[] sendData = new byte[1024];
+      byte[] receiveData = new byte[1024];
+ 
+      String sentence = inFromUser.readLine();
+ 
+      sendData = sentence.getBytes();
+ //sends data
+ 
+      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+      clientSocket.send(sendPacket);
+ 
+      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+      clientSocket.receive(receivePacket);
+ 
+      String modifiedSentence = new String(receivePacket.getData());
+      System.out.println("FROM SERVER:" + modifiedSentence);
+      clientSocket.close();
+   }
+}
